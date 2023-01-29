e0f0cb63991ef51c18f5bc434f11a2b8e10d2cb0
senayakagunduz <senayakgndz@gmail.com>
2023-01-29 15:42:12 +0300

    fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btnEl = document.querySelector("#btn");
 const apiKey = "tLXCQmkdM5twFhQukUzyHA==MXaixEb7F5wBg1lR";
-const jokeEl=document.querySelector("#joke")
+const jokeEl = document.querySelector("#joke");
 
 const options = {
   method: "GET",
@@ -11,14 +11,18 @@ const options = {
 const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke() {
-  jokeEl.innerText="Updating...";
-  btnEl.disabled=true;
-  btnEl.innerText="Loading...";
-  const response = await fetch(apiUrl, options);
-  const data = await response.json();
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
 
-  btnEl.disabled=false;
-  btnEl.innerText="Tell me a joke";
-  jokeEl.innerText=data[0].joke;
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText="There is an error, please try again..."
+  }
 }
 btnEl.addEventListener("click", getJoke);
